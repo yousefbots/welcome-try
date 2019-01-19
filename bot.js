b5ab8f0dc71077e551265fd7aa111cc66b589750
yousefbots <45495875+yousefbots@users.noreply.github.com>
2019-01-19 22:13:08 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+ 
+ 
+ 
+ 
+ 
+ 
+
+     client.on('guildMemberAdd', member => {
+            let channel = member.guild.channels.find('name', 'welcome');
+            let memberavatar = member.user.avatarURL
+            if (!channel) return;
+            let embed = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setThumbnail(memberavatar)
+                .addField('🎽 | name :  ',`${member}`)
+                .addField('📢 | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
+                .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
+                        .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+                   
+                        .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                           
+                                            .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                           
+            .setFooter(`${member.guild.name}`)
+                .setTimestamp()
+       
+            channel.sendEmbed(embed);
+            });
+           
+            client.on('guildMemberRemove', member => {
+                var embed = new Discord.RichEmbed()
+                .setAuthor(member.user.username, member.user.avatarURL)
+                .setThumbnail(member.user.avatarURL)
+                .setTitle(`الله معاك ✋:skin-tone-1: 😔`)
+                .setDescription(`مع السلامه تشرفنا بك ✋:skin-tone-1: 😔 `)
+                .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+                .setColor('RED')
+                .setFooter(`==== نــتــمــنــآ لــكــم آســتــمـــتــآع ====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
+           
+            var channel =member.guild.channels.find('name', 'welcome')
+            if (!channel) return;
+            channel.send({embed : embed});
+            })
+ 
+
+ 
+
+ 
+ 
+client.login('');// توكن البوت
